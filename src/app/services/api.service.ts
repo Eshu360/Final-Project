e6b54a6953f70eb94238d5bbb66fc1d70d4f451d
jagadishjay <jagadishbabu95@gmail.com>
2022-07-30 17:54:52 +0530

    admin access added

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -5,6 +5,11 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
+  resetData() {
+    throw new Error('Method not implemented.');
+  }
+  admin: any;
+  email:any;
 
   constructor(private http: HttpClient) { }
   postProduct(data : any){
@@ -22,12 +27,37 @@ export class ApiService {
  }
 
  isLoggedIn(){
+  console.log(localStorage.getItem('email'))
   return !!localStorage.getItem('session');
+
  }
-//  isAdmin(){
-//   let role=localStorage.getItem('role');
-//   console.log(role);
-//   return (role=='admin'?true:false)
-//  }
+ isAdmin(){
+  let email:any=localStorage.getItem('email');
+  email=(JSON.parse(email))
+  console.log(email)
+  if (email ==="admin@gmail.com"){
+    console.log("admin found")
+    return true
+  }else{
+    console.log(" admin not found")
+    return false
+  }
+
+
+ }
+
+
+// email:any;
+// password:any
+// createToken(){
+//   let admin:any=localStorage.getItem('credentials')
+//     this.admin=JSON.parse(admin)
+//   if (this.email=='admin@gmail.com' && this.password=='admin123'){
+//     console.log("token found")
+//     return true
+//   }
+//   console.log("admin not found")
+//   return false
+// }
 
 }
